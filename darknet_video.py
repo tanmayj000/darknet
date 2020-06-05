9a812bbb49a05eb5f1860195bfae42e217fd4d61
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2020-06-06 02:45:31 +0530

    Update darknet_video.py

--- darknet_video.py
+++ darknet_video.py
@@ -177,7 +177,7 @@ def YOLO(video_path = '/content/mask_footage.mp4', configPath = "cfg/custom-yolo
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
     while True:
-        #try:
+        try:
             prev_time = time.time()
             ret, frame_read = cap.read()
             frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
@@ -197,7 +197,7 @@ def YOLO(video_path = '/content/mask_footage.mp4', configPath = "cfg/custom-yolo
             io.imshow(image)
             io.show()
             cv2.waitKey(3)
-        #except:
+        except:
             break;
       
     cap.release()
